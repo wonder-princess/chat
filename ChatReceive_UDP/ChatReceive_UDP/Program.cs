7e9105e4912d6e990a9deb3dfca6cc426037848c
wonder-princess <54895094+wonder-princess@users.noreply.github.com>
2019-11-18 22:25:11 +0900

    Add files via upload

---
+++ ChatReceive_UDP/ChatReceive_UDP/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ChatReceive_UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //バインドするローカルIPとポート番号
+            string localIpString = "192.168.0.7";
+            var localAddress = System.Net.IPAddress.Parse(localIpString);
+            
+            // Browsing datagram responses of NetBIOS over TCP/IP
+            int localPort = 138;
+            
+            // Browsing requests of NetBIOS over TCP/IP
+            //int localPort = 137;
+
+            //指定したアドレスとポート番号を使用して、IPEndPointクラスの新しいインスタンスを初期化
+            var localEP = new System.Net.IPEndPoint(localAddress, localPort);
+
+            //インスタンスを初期化し、指定したローカルエンドポイントにバインド
+            var udp = new System.Net.Sockets.UdpClient(localEP);
+
+            for (; ; )
+            {
+                //データを受信
+                System.Net.IPEndPoint remoteEP = null;
+
+                //リモートホストが送信したUDPデータグラムを返す
+                byte[] rcvBytes = udp.Receive(ref remoteEP);
+
+                //データを文字列に変換
+                string rcvMsg = System.Text.Encoding.UTF8.GetString(rcvBytes);
+
+                //受信したデータと送信者の情報を表示
+                Console.WriteLine("受信したデータ:{0}", rcvMsg);
+                Console.WriteLine("送信元アドレス:{0}/ポート番号:{1}", remoteEP.Address, remoteEP.Port);
+
+                //"exit"を受信したら終了
+                if (rcvMsg.Equals("exit"))
+                {
+                    break;
+                }
+            }
+
+            //UdpClientを閉じる
+            udp.Close();
+
+            Console.WriteLine("終了しました。");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ChatSend_UDP/ChatSend_UDP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ChatSend_UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //データを送信するリモートホストとポート番号
+            string remoteHost = "192.168.0.7";
+
+            // Browsing datagram responses of NetBIOS over TCP/IP
+            int remotePort = 138;
+
+            // Browsing requests of NetBIOS over TCP/IP
+            //int remotePort = 137;
+
+            //UdpClientオブジェクトを作成する
+            var udp = new System.Net.Sockets.UdpClient();
+
+            for (; ; )
+            {
+                //送信するデータを作成する
+                Console.WriteLine("送信する文字列を入力してください。");
+                string sendMsg = Console.ReadLine();
+                byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(sendMsg);
+
+                //リモートホストを指定してデータを送信する
+                udp.Send(sendBytes, sendBytes.Length, remoteHost, remotePort);
+
+                //"exit"と入力されたら終了
+                if (sendMsg.Equals("exit"))
+                {
+                    break;
+                }
+            }
+
+            //UdpClientを閉じる
+            udp.Close();
+
+            Console.WriteLine("終了しました。");
+            Console.ReadLine();
+        }
+    }
+}
